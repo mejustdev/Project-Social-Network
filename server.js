de331e5c4d0645107e06e5c95135a807a537777e
20twa07 <48910851+20twa07@users.noreply.github.com>
2019-10-29 13:22:24 +0100

    define routes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const connectDB = require('./config/db');
 
 const app = express();
@@ -6,7 +7,16 @@ const app = express();
 // Connect  Database
 connectDB();
 
+// Defined endpoint
 app.get('/', (req, res) => res.send('API running'));
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.port || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+// Server.js is our main entry file.
