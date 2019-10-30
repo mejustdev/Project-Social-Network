f700c87ae10b567c50ac80924b45828e131c30ca
20twa07 <48910851+20twa07@users.noreply.github.com>
2019-10-30 11:38:26 +0100

    use middleware to get user

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-//  @Route   api/auth
+const User = require('../../models/User');
+//  @Route   GET api/auth
 //  @desc    Test route
 //  @access  Public
 
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    // .select ('-password) that will leave off the password. we dont want to
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
