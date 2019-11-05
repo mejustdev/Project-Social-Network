dfeb642cf07d0ee6d09f94ec764b293faf79be16
20twa07 <48910851+20twa07@users.noreply.github.com>
2019-11-05 16:42:21 +0100

    logout & Navbar Links

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,6 +6,7 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  LOGOUT,
 } from './types';
 import { setAlert } from './alert';
 import setAuthToken from '../utils/setAuthToken';
@@ -88,4 +89,12 @@ export const login = (email, password) => async dispatch => {
       type: LOGIN_FAIL,
     });
   }
+};
+
+// Logout/Clear Profile
+
+export const logout = () => dispatch => {
+  dispatch({
+    type: LOGOUT,
+  });
 };
