a61c1338c6204613e4c701fb4348ce539011b1f8
20twa07 <48910851+20twa07@users.noreply.github.com>
2019-11-06 23:18:12 +0100

    Add education & experiences

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from '../actions/types';
+import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE } from '../actions/types';
 
 const initialState = {
   // it holds individual profile
@@ -15,6 +15,7 @@ export default function(state = initialState, action) {
 
   switch (type) {
     case GET_PROFILE:
+    case UPDATE_PROFILE:
       return {
         ...state,
         profile: payload,
