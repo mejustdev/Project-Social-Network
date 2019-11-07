31c66afa8b27ac5c25fe2479e6bf5e1669c1ba52
20twa07 <48910851+20twa07@users.noreply.github.com>
2019-11-07 11:31:39 +0100

    add remove user posts

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,7 @@ const { check, validationResult } = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 const request = require('request');
 const config = require('config');
 
@@ -158,7 +159,8 @@ router.get('/user/:user_id', async (req, res) => {
 // we added auth middleware since it is private
 router.delete('/', auth, async (req, res) => {
   try {
-    // @todo - remove users posts
+    // Remove user posts
+    await Post.deleteMany({ user: req.user.id });
 
     // if condition {...} matches, Remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
