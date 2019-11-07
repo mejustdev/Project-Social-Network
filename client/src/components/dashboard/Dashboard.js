e5313396c7ef5ad156c64466b7ca8d16d9b29183
20twa07 <48910851+20twa07@users.noreply.github.com>
2019-11-07 11:31:53 +0100

    Add Delete Education, Experiences & Account

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,13 +2,18 @@ import React, { useEffect, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { getCurrentProfile } from '../../actions/profile';
+import { getCurrentProfile, deleteAccount } from '../../actions/profile';
 import DashboardActions from './DashboardActions';
 import Spinner from '../layout/Spinner';
 import Experience from './Experience';
 import Education from './Education';
 
-const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
+const Dashboard = ({
+  getCurrentProfile,
+  deleteAccount,
+  auth: { user },
+  profile: { profile, loading },
+}) => {
   useEffect(() => {
     getCurrentProfile();
   }, [getCurrentProfile]);
@@ -25,6 +30,10 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
           <DashboardActions />
           <Experience experience={profile.experience} />
           <Education education={profile.education} />
+          <div className='my-2'></div>
+          <button className='btn btn-danger' onClick={() => deleteAccount()}>
+            <i className='fas fa-user-minus'></i>Delete My Account
+          </button>
         </Fragment>
       ) : (
         <Fragment>
@@ -47,9 +56,10 @@ Dashboard.propTypes = {
 const mapStateToProps = state => ({
   auth: state.auth,
   profile: state.profile,
+  deleteAccount: PropTypes.func.isRequired,
 });
 
 export default connect(
   mapStateToProps,
-  { getCurrentProfile },
+  { getCurrentProfile, deleteAccount },
 )(Dashboard);
