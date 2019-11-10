abd21a5832116f5339af2f391409839453cb0313
20twa07 <48910851+20twa07@users.noreply.github.com>
2019-11-10 07:58:13 +0100

    Prepare For Deployment

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
-
 const connectDB = require('./config/db');
-
+const path = require('path');
 const app = express();
 
 // Connect  Database
@@ -10,16 +9,23 @@ connectDB();
 // Init Middleware --> It should allow us to get data from req.body
 app.use(express.json({ extended: false }));
 
-// Defined endpoint
-app.get('/', (req, res) => res.send('API running'));
-
 // Define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-const PORT = process.env.port || 5000;
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 // Server.js is our main entry file.
