f53b35862dfbd9452aedd3cadd4976122e44da5f
Me <mergorgec@gmail.com>
2021-02-27 21:18:48 +0100

    tested for production

---
+++ client/build/precache-manifest.140ba2efe68f469a958b8095e8af76fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c173f5fbfe7bbfc925246d03a021956f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a953b9e56576d268878",
+    "url": "/static/css/main.d8116c63.chunk.css"
+  },
+  {
+    "revision": "1048a8e5a4781cc88e6c",
+    "url": "/static/js/2.833359f7.chunk.js"
+  },
+  {
+    "revision": "9a953b9e56576d268878",
+    "url": "/static/js/main.384a6877.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
