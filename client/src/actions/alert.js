ae6f0e8d6b193a1d3d26bbdf42dcc03bf42c03f2
Me <mergorgec@gmail.com>
2021-02-27 14:13:41 +0100

    update uuidv4

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,11 +1,11 @@
 import { SET_ALERT, REMOVE_ALERT } from './types';
-import uuid from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 // We are able write below code because of the thunk middleware
 // A thunk in this context is a function that can be dispatched to perform async
 // activity and can dispatch actions and read state.
 // This is an action creator that returns a thunk:
-export const setAlert = (msg, alertType, timeout = 5000) => dispatch => {
-  const id = uuid.v4();
+export const setAlert = (msg, alertType, timeout = 5000) => (dispatch) => {
+  const id = uuidv4();
   dispatch({
     type: SET_ALERT,
     payload: { msg, alertType, id },
