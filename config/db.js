4bea4f7d79c48718432edd0292a723bc12d106ab
Me <mergorgec@gmail.com>
2022-02-21 23:59:12 +0100

    redeploy

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-// We created this function because We need sth to call in server.js
+
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
