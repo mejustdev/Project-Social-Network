f89b815eb060eb463ca29e9087aa1e5b3c43b65e
Me <mergorgec@gmail.com>
2022-03-06 23:05:33 +0100

    redeploy

--- server.js
+++ server.js
@@ -27,5 +27,3 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-// Server.js is our main entry file.
